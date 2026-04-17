24f016e92b82836df00d01825e1c33912e1cb5a1
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-17 14:51:26 -0500

    Update retroarch_setup.py

--- retroarch_setup.py
+++ retroarch_setup.py
@@ -2,7 +2,7 @@ import os
 
 os.system("sudo apt update && sudo apt install p7zip-full wget nginx git -y")
 
-os.system("wget https://buildbot.libretro.com/stable/1.17.0/emscripten/RetroArch.7z")
+os.system("wget https://buildbot.libretro.com/stable/1.22.2/emscripten/RetroArch.7z")
 os.system("7z x RetroArch.7z")
 os.mkdir("emu")
 os.chdir("emu")
