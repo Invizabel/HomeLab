da5d3c8d02dd421d978db10ccb61c7e1f676b4c5
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-04-17 13:01:53 -0500

    Create plainchess_setup.py

---
+++ plainchess_setup.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("sudo apt update && sudo apt install nginx git -y")
+os.system("git clone https://github.com/TimWoelfle/PlainChess")
+os.system("sudo cp -r PlainChess/* /var/www/html/")
