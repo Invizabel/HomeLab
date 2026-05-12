85383d4b20c4b66327e9b061d3340d9bed1db4a6
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-05-12 08:50:44 -0500

    Update docker_setup.py

--- Assets/docker_setup.py
+++ Assets/docker_setup.py
@@ -2,7 +2,7 @@ import os
 
 # Ubuntu Setup Only
 os.system("sudo apt update")
-os.system("sudo apt install apt-transport-https curl")
+os.system("sudo apt install ca-certificates curl")
 os.system("sudo install -m 0755 -d /etc/apt/keyrings")
 os.system("sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc")
 os.system('echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo ${UBUNTU_CODENAME:-$VERSION_CODENAME}) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null')
