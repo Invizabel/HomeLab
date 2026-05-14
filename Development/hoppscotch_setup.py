a1f5d09ffbb958ee8b54afe21c401213387f7e3d
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-05-14 09:59:27 -0500

    Add files via upload

---
+++ Development/hoppscotch_setup.py
@@ -0,0 +1,8 @@
+import os
+
+os.mkdir("app")
+os.chdir("app")
+os.system("sudo apt update && sudo apt install wget -y")
+os.system("wget https://raw.githubusercontent.com/Invizabel/HomeLab/refs/heads/main/Assets/docker_setup.py")
+os.system("python3 docker_setup.py")
+os.system("sudo docker run -d --name=hoppscotch --restart always -p 80:3100 hoppscotch/hoppscotch")
