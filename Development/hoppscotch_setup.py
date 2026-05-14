6fb5c329dbba8d0da88c402bb3ce958dd371f537
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-05-14 10:24:03 -0500

    Delete Development/hoppscotch_setup.py

--- Development/hoppscotch_setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.mkdir("app")
-os.chdir("app")
-os.system("sudo apt update && sudo apt install wget -y")
-os.system("wget https://raw.githubusercontent.com/Invizabel/HomeLab/refs/heads/main/Assets/docker_setup.py")
-os.system("python3 docker_setup.py")
-os.system("sudo docker run -d --name=hoppscotch -p 3000:3000 --restart always hoppscotch/hoppscotch:latest")
