f4e2062abe996196c0eeb0e94e6aba773fb58554
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-06-19 15:16:53 -0500

    Add files via upload

--- Smart Home/Arduino/soil_moisture/soil_moisture.ino
+++ Smart Home/Arduino/soil_moisture/soil_moisture.ino
@@ -1,7 +1,6 @@
 #define PIEZO_PIN  9 // Pin connected to the piezo buzzer.
 
 int tone_freq = 400;
-int tone_count = 8;
 
 void setup()
 {
@@ -12,14 +11,10 @@ void loop()
 {
   while (true)
   {
-    // Beep piezo three times if plants need wanter (not implemented yet)
-    for (int i = 0; i < tone_count; i++)
-    {
-      tone(PIEZO_PIN, tone_freq);
-      delay(1000);
-      noTone(PIEZO_PIN);
-      delay(1000);
-    }
+    // Beep piezo if plants need wanter (not implemented yet)
+    tone(PIEZO_PIN, tone_freq);
+    delay(500);
+    noTone(PIEZO_PIN);
 
     // Wait 15 minutes before testing soil moisture and beep the Piezo if required
     delay(900000);
