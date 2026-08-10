36a2594b103036fd429f6ec035b46f0eea531b8e
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-08-09 20:30:03 -0500

    Update chess_setup.py

--- Games/chess_setup.py
+++ Games/chess_setup.py
@@ -6,4 +6,4 @@ os.system("sudo apt update && sudo apt install nginx git php-cli php-fpm php-com
 sudo.system('sudo systemctl restart "php$(php -r \'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;\')-fpm"')
 os.system("git clone https://github.com/TimWoelfle/PlainChess")
 os.system("sudo cp -r PlainChess/* /var/www/html/")
-os.system("PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;'); sudo tee -a /etc/nginx/sites-available/default >/dev/null <<EOF location ~ \.php$ { include snippets/fastcgi-php.conf; fastcgi_pass unix:/run/php/php${PHP_VERSION}-fpm.sock; } EOF")
+os.system("""PHP_VERSION=$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;'); printf '\\nlocation ~ \\.php$ {\\n    include snippets/fastcgi-php.conf;\\n    fastcgi_pass unix:/run/php/php%s-fpm.sock;\\n}\\n' "$PHP_VERSION" | sudo tee -a /etc/nginx/sites-available/default >/dev/null""")
