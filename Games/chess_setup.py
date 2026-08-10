99d7c17f789ae2032296e1aed1fb723338f6422e
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-08-09 20:20:21 -0500

    Update chess_setup.py

--- Games/chess_setup.py
+++ Games/chess_setup.py
@@ -2,6 +2,7 @@ import os
 
 os.mkdir("app")
 os.chdir("app")
-os.system("sudo apt update && sudo apt install nginx git -y")
+os.system("sudo apt update && sudo apt install nginx git php-cli php-fpm php-common -y")
+sudo.system('sudo systemctl restart "php$(php -r 'echo PHP_MAJOR_VERSION.".".PHP_MINOR_VERSION;')-fpm"')
 os.system("git clone https://github.com/TimWoelfle/PlainChess")
 os.system("sudo cp -r PlainChess/* /var/www/html/")
